ee8a633dea899889ed6d58846c98c19bc14f1017
Viktor Schastnyy <victor92sh@gmail.com>
2021-11-01 10:45:39 +0300

    Added user proto files

--- generate.go
+++ generate.go
@@ -1,4 +1,5 @@
 //go:generate protoc --proto_path=auth --go_out=./auth --go_opt=paths=source_relative --go-grpc_out=./auth --go-grpc_opt=paths=source_relative auth.proto
 //go:generate protoc --proto_path=captcha --go_out=./captcha --go_opt=paths=source_relative --go-grpc_out=./captcha --go-grpc_opt=paths=source_relative captcha.proto
+//go:generate protoc --proto_path=user --go_out=./user --go_opt=paths=source_relative --go-grpc_out=./user --go-grpc_opt=paths=source_relative user.proto
 
 package generate
